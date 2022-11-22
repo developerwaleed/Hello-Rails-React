01074b7720b02084c876a5297a479475e5ab09df
Waleed Amjad <waleedamjad99@hotmail.com>
2022-11-22 15:22:55 +0500

    Setup Rubocop

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,8 +1,7 @@
-class Api::V1::GreetingsController <  ActionController::API
-    def random
-      greetings = Greeting.all
-      @greeting = greetings[rand(greetings.size)]
-      render json: @greeting
-    end
+class Api::V1::GreetingsController < ActionController::API
+  def random
+    greetings = Greeting.all
+    @greeting = greetings[rand(greetings.size)]
+    render json: @greeting
   end
-  
+end
